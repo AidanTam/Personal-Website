98f9456b72755690b94c9b3d8dbbaeecd91913b5
AidanTam <atam243@uwo.ca>
2026-01-02 12:11:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-Website/", 
+  // Changed from "/Personal-Website/" to "/" because you are now using aidantam.ca
+  base: "/", 
 })
