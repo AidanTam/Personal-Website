b7c3be8083165a45d95c5b3c7c5c676a1667ff1d
AidanTam <atam243@uwo.ca>
2026-01-02 12:20:39 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Changed from "/Personal-Website/" to "/" because you are now using aidantam.ca
-  base: "/", 
+  // Use './' (dot-slash) to make paths relative
+  base: './', 
 })
